32dd9d04b4b1b744d2f0d6ec77651d663690a23d
rishi-07-web <rishikumar1753@gmail.com>
2025-02-04 15:09:44 +0530

    iiitb_rv32i_tb.v

--- iiitb_rv32i_tb.v
+++ iiitb_rv32i_tb.v
@@ -1,26 +1,23 @@
 module iiitb_rv32i_tb;
 
-reg clk, RN;
-wire [31:0] WB_OUT, NPC;
+reg clk,RN;
+wire [31:0]WB_OUT,NPC;
 
-iiitb_rv32i rv32(clk, RN, NPC, WB_OUT);
+iiitb_rv32i rv32(clk,RN,NPC,WB_OUT);
 
-always #3 clk = !clk;
 
-initial begin
-    // Initialize reset signal and clock
-    RN = 1'b1;
-    clk = 1'b1;
+always #3 clk=!clk;
 
-    // Initialize dumpfile for simulation trace
-    $dumpfile("iiitb_rv32i.vcd");
-    $dumpvars(0, iiitb_rv32i_tb);
+initial begin 
+RN  = 1'b1;
+clk = 1'b1;
 
-    // Apply reset
-    #5 RN = 1'b0;
+$dumpfile ("iiitb_rv32i.vcd"); //by default vcd
+$dumpvars (0, iiitb_rv32i_tb);
+  
+  #5 RN = 1'b0;
+  
+  #300 $finish;
 
-    // Run the simulation for a given time
-    #300 $finish;
 end
-
 endmodule
